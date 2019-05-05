f5fc13cdb468e408216b6da25e44b96773f46ce4
Fadia04 <fl.medja@gmail.com>
2019-05-06 01:15:30 +0200

    Add test1 et test2

---
+++ test1.py
@@ -0,0 +1,22 @@
+
+
+
+n = 1
+while n < 3:
+    print(n)
+    n+=1
+
+
+n = 1
+while n < 3:
+     print(n, "+", n, "=", n + n)
+     print(n, "-", n, "=", n - 1)
+     print(n, "/", n, "=", n / n)
+     print(n, "*", n, "=", n * n)
+     n += 1
+    
+
+
+
+
+
---
+++ test2.py
@@ -0,0 +1,9 @@
+
+
+a = 1
+b = 2
+
+print(a + a, a + b, b + b, b + a)
+print(a - a, a - b, b - b, b - a)
+print(a / a, a / b, b / b, b / a)    
+print(a * a, a * b, b * b, b * a)
