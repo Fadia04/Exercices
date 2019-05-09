00c3f7277628a4455ce0d05723ec8518fc71f1c8
Fadia04 <fl.medja@gmail.com>
2019-05-09 10:03:18 +0200

    test6

---
+++ test6.py
@@ -0,0 +1,14 @@
+# Ecrire un programme qui permet de 
+# saisir un nombre puis déterminer 
+# s'il appartient à un intervalle donné,
+#  sachant que les extrémités de 
+# l'intervalle sont fixées par l'utilisateur.
+
+
+n = input("Entrez une donnée numérique")
+n == 5>n<20
+n = int(n)
+if n > 5 and n < 20:
+    print("n est dans l'intervale")
+else:
+    print("n n'est pas dans l'intervalle")
