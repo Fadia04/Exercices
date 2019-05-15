44725f9dd82bf4ebe548663fb160fc010c45a3f5
Fadia04 <fl.medja@gmail.com>
2019-05-15 17:55:27 +0200

    Add test_7

---
+++ test_7.py
@@ -0,0 +1,16 @@
+# Ecrire un programme qui demande deux 
+# nombres à l’utilisateur etl’informe
+#  ensuite si leur produit est négatif 
+# ou positif. Attention toutefois :on 
+# ne doit pas calculer le produit des
+# deux nombres.
+
+n1 = input("Entrez une donnée numérique")
+n2 = input("Entrez une donnée numérique")
+n1 = int(n1)
+n2 = int(n2)
+
+if (n1 * n2) < 0:
+    print("p est négatif")
+else:
+    print("p est positif")
