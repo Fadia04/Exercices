9ccbc0cfbc1c4369ed2e90d410262e1a5b514528
Fadia04 <fl.medja@gmail.com>
2019-05-15 17:51:07 +0200

    Add test_1

---
+++ test_1.py
@@ -0,0 +1,17 @@
+
+
+
+n = 1
+while n < 3:
+    print(n)
+    n+=1
+
+
+n = 0
+while n < 3:
+    print(n, "+", n, "=", n + n)
+    print(n, "-", n, "=", n - 1)
+    print(n, "*", n, "=", n * n)
+    if n != 0:
+        print(n, "/", n, "=", n / n)
+    n += 1
